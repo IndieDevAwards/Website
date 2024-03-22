44bf1f2a5af18c692576e96429acbcb067e77ac1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-03-22 10:35:03 -0700

    delete build folder

---
+++ Sources/Pages/Examples/ButtonExamples.swift
@@ -0,0 +1,17 @@
+//
+// ButtonExamples.swift
+// IndieDevAwardsWebsite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+import Foundation
+import Ignite
+
+struct ButtonExamples: StaticPage {
+    var title = "Buttons"
+
+    func body(context: PublishingContext) -> [BlockElement] {
+
+    }
+}
